318e7cdb623fbe76322801d432a6e86cb336ab82
Jockey780 <xlordhitmanx@gmail.com>
2024-06-06 15:56:58 +0700

    CRUD for JoinAuction

---
+++ JewelryAuctionWebApp/Controllers/JoinAuctionsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JewelryAuctionData.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace JewelryAuctionWebApp.Controllers
+{
+    public class JoinAuctionsController : Controller
+    {
+        private string apiUrl = "https://localhost:7169/api/JoinAuction/";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JoinAuctionsController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<List<JoinAuction>> GetAll()
+        {
+            try
+            {
+                var result = new List<JoinAuction>();
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync(apiUrl + "GetAll"))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            result = JsonConvert.DeserializeObject<List<JoinAuction>>(content);
+                        }
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetJoinAuctionById(int joinAuctionId)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(apiUrl + "GetById?id=" + joinAuctionId);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var joinAuction = JsonConvert.DeserializeObject<JoinAuction>(content);
+                        return Json(joinAuction);
+                    }
+                    else
+                    {
+                        return Json(null);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+        //    return PartialView("Create", new JoinAuction());
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] JoinAuction joinAuction)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var jsonContent = new StringContent(JsonConvert.SerializeObject(joinAuction), Encoding.UTF8, "application/json");
+                    using (var response = await httpClient.PostAsync(apiUrl + "Create", jsonContent))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            return Json(new { status = 1, message = "Auction created successfully." });
+                        }
+                        else
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            return Json(new { status = 0, message = "Failed to create auction." });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = 0, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update([FromBody] JoinAuction joinAuction)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var jsonContent = new StringContent(JsonConvert.SerializeObject(joinAuction), Encoding.UTF8, "application/json");
+                    using (var response = await httpClient.PostAsync(apiUrl + "Update", jsonContent))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            return Json(new { status = 1, message = "Auction updated successfully." });
+                        }
+                        else
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            return Json(new { status = 0, message = "Auction to update payment." });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = 0, message = ex.Message });
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int joinAuctionId)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.DeleteAsync(apiUrl + "Delete?joinAuctionId=" + joinAuctionId);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return Json(new { status = 1, message = "Deleted successfully." });
+                    }
+                    else
+                    {
+                        return Json(new { status = 0, message = "Failed to delete." });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = 0, message = ex.Message });
+            }
+        }
+    }
+}
